db4b579492c01440ca478fc7f143ee1c63ad4d45
Sabit <sabit.mussabek@gmail.com>
2024-03-25 19:01:51 +0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sabit Mussabek on 22.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
